86e2ab33cbc0a8fa84c248cffc4c4028e1a9c6ff
samshum2 <39322960+samshum2@users.noreply.github.com>
2021-03-19 10:23:06 +0800

    Update AutoSyncTime_Win.py
    Debug:
    It can correctly run in all day.
    The last bug:
    the time may be cal to < -1 or >= 24 ,it will

--- AutoSyncTime_Win.py
+++ AutoSyncTime_Win.py
@@ -6,7 +6,8 @@ import ntplib
 import time
 try:
 	x = ntplib.NTPClient()
-	timeStamp = x.request('uk.pool.ntp.org').tx_time
+	# timeStamp = x.request('uk.pool.ntp.org').tx_time
+	timeStamp = x.request('uk.pool.ntp.org').tx_time  # get the time in uk server
 	dateData = time.strftime("%Y %m %d %H %M %S", time.localtime(timeStamp))
 	year = time.strftime("%Y", time.localtime(timeStamp))
 	year,mouth,day,hour,minutes,second = dateData.split()
@@ -17,10 +18,18 @@ try:
 		except:
 			return False
 	if is_admin():
-		win32api.SetSystemTime(year,mouth,3,day,hour-8,minutes,second,0)
+		print((year,mouth,3,day,hour,minutes,second,0))
+		if hour<8: # hong kone time zone is +8
+			hour = hour + 16  # 16 is = 24 - 8
+			day = day - 1 # you may change here too , if your time zone >= +1 use -1 day, if your time zone <= -1 use +1 day ?
+		else:
+			print('else')
+			hour = hour - 8
+		win32api.SetSystemTime(year,mouth,3,day,hour,minutes,second,0)
+		# win32api.SetSystemTime(year,mouth,3,day,hour-8,minutes,second,0)
 	else:
 		ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
 except:
 	traceback.print_exc()
 finally:
-	time.sleep(10)
+	time.sleep(3)
